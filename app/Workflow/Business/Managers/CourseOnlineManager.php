93b9a32603564a60d390a95f7da2fe69518c2dfb
Lvsi-China <15239928381@163.com>
2018-04-05 22:06:03 +0800

    no message

--- app/Workflow/Business/Managers/CourseOnlineManager.php
+++ app/Workflow/Business/Managers/CourseOnlineManager.php
@@ -24,40 +24,16 @@ class CourseOnlineManager extends BaseManager
     {
 
         $data = self::$businessData;
-
         CourseOnlineService::run($production2, 'processXXXX');
     }
 
     public function register()
     {
-
-        // 永远只有这些操作
-        $production1 = (new AService())->run();
-        $production2 = (new BService())->run($production1);
-        $production3 = (new CService())->run($production2);
-
-        // 把一个新任务放到队列中
-        pushJobIntoQueue($production3, SendEMailQueue::class);
-
-        $production = $production1 + $production2 + $production3;
-
-        Memory::set('workflow.business.data', $production);
     }
 
     public function index()
     {
 
-        $production1 = (new AService())->run();
-        $production2 = (new ZService())->run($production1);
-        $production3 = (new CService())->run($production2);
-
-
-        // 把一个新任务放到队列中
-        pushJobIntoQueue($production3, SendEMailQueue::class);
-
-        $production = $production1 + $production2 + $production3;
-
-        Memory::set('workflow.business.data', $production);
     }
 
 
